ae69d12945251c39c6c8f3cc94a5882383223688
C Harindra <122661512+Fullstackhari@users.noreply.github.com>
2026-09-08 13:41:56 +0530

    Update MyntraWishListVerification.java

--- Day1_Assesment/MyntraWishListVerification.java
+++ Day1_Assesment/MyntraWishListVerification.java
@@ -17,22 +17,27 @@ public class MyntraWishListVerification {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://www.myntra.com/");
 		Thread.sleep(2000);
+		//clicking on wishlist to access the login page
 		driver.findElement(By.xpath("//span[text()='Wishlist']")).click();
 		Thread.sleep(10000);
 		driver.findElement(By.cssSelector("[class='wishlistLogin-button']")).click();
 		Thread.sleep(10000);
+		//logging in using Phone Number
 		driver.findElement(By.cssSelector("[class='form-control mobileNumberInput']")).sendKeys("8867554866");
 		Thread.sleep(1000);
 		driver.findElement(By.cssSelector("[type='checkbox']")).click();
 		driver.findElement(By.cssSelector("[class='submitBottomOption']")).click();
 		Thread.sleep(50000);
-		driver.findElement(By.cssSelector("[class='submitBottomOption']")).click();
+		driver.findElement(By.cssSelector("[class='submitBottomOption']")).click(); // after click action provide the OTP Manually
 		Thread.sleep(50000);
+		//clicking on continue shopping to resume the shopping
 		driver.findElement(By.cssSelector("[class=\"wishlistEmpty-button\"]")).click();
 		Thread.sleep(5000);
+		//searching the product (shirts)
 		driver.findElement(By.xpath("//input[@class='desktop-searchBar']")).sendKeys("Shirts");
 		Thread.sleep(2000);
 		List<WebElement> sugg1 = driver.findElements(By.cssSelector("[class='desktop-group']"));
+		//selecting the suggestion 
 		Thread.sleep(1000);
 		for(WebElement ele:sugg1) {
 			if(ele.getText().contains("Men Casual")) {
@@ -42,12 +47,15 @@ public class MyntraWishListVerification {
 		Thread.sleep(2000);
 		WebElement products1 = driver.findElement(By.cssSelector("[class=\"results-base\"]"));
 		Actions ac1=new Actions(driver);
+		//mouse hovering on the product to make the wishlist button to visible
 		ac1.moveToElement(products1,20,30).perform();
 		Thread.sleep(1000);
+		//clicking on wishlist button
 		driver.findElement(By.xpath("(//span[@class='product-wishlistFlex product-actionsButton product-wishlist '])[3]")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[text()='Wishlist']")).click();
 		Thread.sleep(10000);
+		//validation
 		List<WebElement> wishlistProducts =
 		        driver.findElements(By.className("itemcard-removeIcon"));
 
