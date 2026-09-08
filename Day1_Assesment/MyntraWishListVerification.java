c719cfbf018d0f58036968e939562f8b1b79e37b
Fullstackhari <harindracs12@gmail.com>
2026-09-08 13:34:43 +0530

    Updated Myntra wishlist assessment

--- Day1_Assesment/MyntraWishListVerification.java
+++ Day1_Assesment/MyntraWishListVerification.java
@@ -17,30 +17,45 @@ public class MyntraWishListVerification {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://www.myntra.com/");
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@class='desktop-searchBar']")).sendKeys("Shirts");
-		Thread.sleep(2000);
-		List<WebElement> sugg = driver.findElements(By.cssSelector("[class='desktop-group']"));
-		Thread.sleep(1000);
-		for(WebElement ele:sugg) {
-			if(ele.getText().contains("Men Casual")) {
-				ele.click();
-			}
-		}
-		Thread.sleep(2000);
-		WebElement products = driver.findElement(By.cssSelector("[class=\"results-base\"]"));
-		Actions ac=new Actions(driver);
-		ac.moveToElement(products,20,30).perform();
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("(//span[@class='product-wishlistFlex product-actionsButton product-wishlist '])[3]")).click();
-		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[text()='Wishlist']")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.cssSelector("[class='wishlistLogin-button']")).click();
 		Thread.sleep(10000);
 		driver.findElement(By.cssSelector("[class='form-control mobileNumberInput']")).sendKeys("8867554866");
 		Thread.sleep(1000);
 		driver.findElement(By.cssSelector("[type='checkbox']")).click();
 		driver.findElement(By.cssSelector("[class='submitBottomOption']")).click();
-		Thread.sleep(80000);
-		
+		Thread.sleep(50000);
+		driver.findElement(By.cssSelector("[class='submitBottomOption']")).click();
+		Thread.sleep(50000);
+		driver.findElement(By.cssSelector("[class=\"wishlistEmpty-button\"]")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@class='desktop-searchBar']")).sendKeys("Shirts");
+		Thread.sleep(2000);
+		List<WebElement> sugg1 = driver.findElements(By.cssSelector("[class='desktop-group']"));
+		Thread.sleep(1000);
+		for(WebElement ele:sugg1) {
+			if(ele.getText().contains("Men Casual")) {
+				ele.click();
+			}
+		}
+		Thread.sleep(2000);
+		WebElement products1 = driver.findElement(By.cssSelector("[class=\"results-base\"]"));
+		Actions ac1=new Actions(driver);
+		ac1.moveToElement(products1,20,30).perform();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("(//span[@class='product-wishlistFlex product-actionsButton product-wishlist '])[3]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()='Wishlist']")).click();
+		Thread.sleep(10000);
+		List<WebElement> wishlistProducts =
+		        driver.findElements(By.className("itemcard-removeIcon"));
+
+		if (wishlistProducts.size() > 0) {
+		    System.out.println("PASS: Product is added to Wishlist");
+		} else {
+		    System.out.println("FAIL: Product is NOT added to Wishlist");
+		}
 
 	}
 
